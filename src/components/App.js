4678ef214e46cf02ff5aa63cbfc50539143572d6
iamkennethreyt <keba.tamis@swu.edu.ph>
2018-09-16 13:24:03 +0800

    initialize my web

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,87 @@
-import React from 'react'
+import React from "react";
+import "../styles/styles.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>HELLO WORLD</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header class="showcase">
+          <div class="content">
+            <img
+              src="https://image.ibb.co/d4EAuz/Tamis_Kenneth_Rey.jpg"
+              class="logo"
+              alt="Traversy Media"
+            />
+            <div class="title">Kenneth Rey Tamis</div>
+            <div class="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+              vel.
             </div>
-        )
-    }
+          </div>
+        </header>
+
+        <section class="services">
+          <div class="container grid-3 center">
+            <div>
+              <a href="https://github.com/iamkennethreyt">
+                <i class="fab fa-github fa-3x" />
+                <h3>GitHub</h3>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+                reiciendis!
+              </p>
+            </div>
+            <div>
+              <a href="https://facebook.com/iamkennethreyt">
+                <i class="fab fa-facebook-f fa-3x" />
+                <h3>Facebook</h3>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+                reiciendis!
+              </p>
+            </div>
+            <div>
+              <a href="https://instagram.com/iamkennethreyt">
+                <i class="fab fa-instagram fa-3x" />
+                <h3>Instagram</h3>
+              </a>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+                reiciendis!
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section class="about bg-light">
+          <div class="container">
+            <div class="grid-2">
+              <div class="center">
+                <i class="fas fa-laptop-code fa-10x" />
+              </div>
+              <div>
+                <h3>About Us</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
+                  eos aperiam labore consectetur maiores ea magni exercitationem
+                  similique laborum sed, unde, autem vel iure doloribus aliquid.
+                  Aspernatur explicabo consectetur consequatur non nesciunt
+                  debitis quos alias soluta, ratione, ipsa officia reiciendis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer class="center bg-dark">
+          <p>Kenneth Rey Tamis &copy; 2018</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {}
-                  }
-                ]
-            }
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
     ]
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
